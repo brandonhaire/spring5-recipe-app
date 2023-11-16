fc298390211eca1a8cca13959feac88b65814403
Haire Brandon <Brandon.Haire@ngc.com>
2023-11-16 15:34:11 -0500

    Create Index Page

---
+++ src/main/java/guru/springframework/spring5recipeappp/Spring5RecipeApppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5recipeappp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeApppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipeApppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring5recipeappp/Spring5RecipeApppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5recipeappp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeApppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
